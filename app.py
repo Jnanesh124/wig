71eac4135ee7e7b34f8871b3e80595ed1246576d
Jnanesh124 <99884442+Jnanesh124@users.noreply.github.com>
2022-12-27 20:50:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://github.com/Gowdru123/file-store-sp okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://github.com/Jnanesh124/newhop0 okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
